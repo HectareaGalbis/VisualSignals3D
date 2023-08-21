172b51e8bbd7dff0927c3a5e82a71476d6ddf51f
Hectarea <hectometrocuadrado@gmail.com>
2023-08-21 11:06:58 +0200

    Fixed the find receivers method

--- addons/visual_signals_3d/core/signal3d_manager_inspector.gd
+++ addons/visual_signals_3d/core/signal3d_manager_inspector.gd
@@ -3,25 +3,26 @@ extends EditorInspectorPlugin
 var signal_manager_editor_res = preload("res://addons/visual_signals_3d/core/signal3d_manager_editor.gd")
 
 
-
-func _can_handle(object):
-	if Signal3DUtils.is_this_my_scene(object as Node):
-		var signal_receivers = object.owner.find_children("*","Signal3DReceiverBase")
-		return !signal_receivers.is_empty()
-	return false
-
-static func _find_child_receivers(node : Node):
+static func _find_child_receivers(node : Node, root : Node):
 	var receivers : Array[Signal3DReceiverBase] = []
 	var children = node.get_children()
 	for child in children:
-		if child is Signal3DReceiverBase:
-			receivers.push_back(child)
-			receivers.append_array(_find_child_receivers(child))
+		if child.owner == root:
+			if child is Signal3DReceiverBase:
+				receivers.push_back(child)
+			receivers.append_array(_find_child_receivers(child, root))
 	return receivers
+
+
+func _can_handle(object):
+	var is_its_scene = Signal3DUtils.is_this_my_scene(object as Node) # not sure this is needed
+	var has_receivers = !_find_child_receivers(object , object).is_empty()
+	return is_its_scene and has_receivers
+
 
 func _parse_begin(object):
 	var root_node = object as Node
-	var signal_receivers = _find_child_receivers(root_node)
+	var signal_receivers = _find_child_receivers(root_node, root_node)
 	for signal_receiver in signal_receivers:
 		if signal_receiver.owner == object:
 			var signal_manager_editor = signal_manager_editor_res.new(signal_receiver)
